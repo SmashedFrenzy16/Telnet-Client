fef8c89181c8dca5b23789d8e268cb07ae60e015
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-08-07 20:11:54 +0500

    Update telnet_client.py

--- telnet_client.py
+++ telnet_client.py
@@ -10,3 +10,9 @@ telnet_obj = telnetlib.Telnet(HOST, PORT)
 message = ("GET /index.html HTTP/1.1\nHost: "+ HOST +"\n\n").encode('ascii')
 
 telnet_obj.write(message)
+
+output = telnet_obj.read_all()
+
+print(output)
+
+telnet_obj.close()
