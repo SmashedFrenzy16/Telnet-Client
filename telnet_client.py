369d0d466733390a190fce48b23012ae9902eb4b
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-08-07 20:11:06 +0500

    Update telnet_client.py

--- telnet_client.py
+++ telnet_client.py
@@ -1,2 +1,12 @@
 import sys
 import telnetlib
+
+
+HOST = input("Enter host: ")
+PORT = input("Enter port: ")
+
+telnet_obj = telnetlib.Telnet(HOST, PORT)
+
+message = ("GET /index.html HTTP/1.1\nHost: "+ HOST +"\n\n").encode('ascii')
+
+telnet_obj.write(message)
